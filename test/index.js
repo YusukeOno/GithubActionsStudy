1892f0e0296d72b262955639a84d59355f9e1cd1
小野裕輔 <y-ono@nswt.co.jp>
2020-11-19 22:49:53 +0900

    formatting only.

--- test/index.js
+++ test/index.js
@@ -3,20 +3,19 @@ const { it } = require("mocha");
 const fizzbuzz = require("../index");
 
 describe("fizzbuzz", () => {
-    it("returns FizzBuzz when value is divisible by 15", () => {
-        assert(fizzbuzz(30) === "FizzBuzz");
-    });
+  it("returns FizzBuzz when value is divisible by 15", () => {
+    assert(fizzbuzz(30) === "FizzBuzz");
+  });
 
-    it("returns Fizz when value is divisible by 3", () => {
-        assert(fizzbuzz(9) === "Fizz");
-    });
+  it("returns Fizz when value is divisible by 3", () => {
+    assert(fizzbuzz(9) === "Fizz");
+  });
 
-    it("returns Buzz when is divisible by 5", () => {
-        assert(fizzbuzz(10) === "Buzz");
-    });
+  it("returns Buzz when is divisible by 5", () => {
+    assert(fizzbuzz(10) === "Buzz");
+  });
 
-    it("returns the value when value is not divisible by 3 or 5", () => {
-        assert(fizzbuzz(7) === "7");
-    });
-
+  it("returns the value when value is not divisible by 3 or 5", () => {
+    assert(fizzbuzz(7) === "7");
+  });
 });
